266217a80862c667c9a263f4a9dad5cacf545356
iE019 <55221575+iE019@users.noreply.github.com>
2021-06-29 18:21:23 +0800

    Update JPA101.java

--- TQC加Java實戰/第一 類_基本認識/JPA101.java
+++ TQC加Java實戰/第一 類_基本認識/JPA101.java
@@ -1,17 +1,16 @@
 /*字串列印
 設計說明:
-   	  1.修改程式碼片段中的程式語法、邏輯上的錯誤。
+   	1.修改程式碼片段中的程式語法、邏輯上的錯誤。
         執行結果如下:
     		I love Lord Jesus!
-		    God is love!
+		God is love!
  */
 
 public class JPA101{ //Start of class JPA101
   public static void main(String[] args){ // Start of main method
 
-    print("I love Lord Jesus!\n");
+    print("I love Lord Jesus!\n"); // 要列印的字串必須要用雙引號或單引號刮起來 //  \n 代表
     print("God is love!\n");
 
   } // End of main method
-
 } // End of main class JPA101
