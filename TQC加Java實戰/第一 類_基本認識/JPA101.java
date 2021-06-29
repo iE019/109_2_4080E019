f91a302611a5cea10a88642e0ada291509ba3e37
iE019 <55221575+iE019@users.noreply.github.com>
2021-06-29 18:08:43 +0800

    Create JPA101.java

---
+++ TQC加Java實戰/第一 類_基本認識/JPA101.java
@@ -0,0 +1,17 @@
+/*字串列印
+設計說明:
+   	  1.修改程式碼片段中的程式語法、邏輯上的錯誤。
+        執行結果如下:
+    		I love Lord Jesus!
+		    God is love!
+ */
+
+public class JPA101{ //Start of class JPA101
+  public static void main(String[] args){ // Start of main method
+
+    print("I love Lord Jesus!\n");
+    print("God is love!\n");
+
+  } // End of main method
+
+} // End of main class JPA101
